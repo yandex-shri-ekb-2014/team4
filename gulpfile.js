9a6a51836a6dbe5600fce799285414908af4a309
grid <gridsane@gmail.com>
2014-11-22 21:59:46 +0500

    пофиксить и почистить gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,44 @@
-// Include gulp
 var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var browserify = require('gulp-browserify');
+var connect = require('gulp-connect');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
-// Include Our Plugins
-var jshint = require('gulp-jshint'),
-sass = require('gulp-sass'),
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify'),
-rename = require('gulp-rename'),
-autoprefixer = require('gulp-autoprefixer'),
-bf = require('gulp-browserify'),
-ignore = require('gulp-ignore'),
-rimraf = require('gulp-rimraf'),
-livereload = require('gulp-livereload');
-
-//Lint Task
-gulp.task('lint', function() {
-   return gulp.src('app/*.js')
-       .pipe(jshint())
-       .pipe(jshint.reporter('default'));
-});
-
-// Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('src/base.scss')
+    return gulp.src('src/styles/base.scss')
         .pipe(sass())
-        .pipe(rename('main.css'))
+        .pipe(rename('style.css'))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('build/css'));
-
+        .pipe(gulp.dest('assets/css'))
+        .pipe(connect.reload());
 });
 
-// Browserify
-gulp.task('bf', function () {
-    return gulp.src(['app/router.js'], {read: true})
+gulp.task('browserify', function () {
+    return gulp.src(['src/router.js'], {read: true})
         // .pipe(browserify({
         //     debug: true,
         //     transform: ['jstify'] // hbsify, нужно будет погуглить пакет, который за это отвечает, он так и называется вроде
         // }))
         .pipe(rename('compile.js'))
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('assets/js'))
+        .pipe(connect.reload());
 
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('build/js/compile.js')
-        .pipe(rename('compile.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/js'));
+gulp.task('connect', function () {
+    connect.server({
+        livereload: true
+    });
 });
 
-// Move index
-gulp.task('move_html', function(){
-    return gulp.src('src/index.html')
-    .pipe(gulp.dest('build/'));
+gulp.task('watch', function () {
+    gulp.watch(['src/**/*.{js,hbs}'], ['browserify']);
+    gulp.watch(['src/**/*.scss'], ['sass']);
 });
 
-// Move images
-gulp.task('move_images', function(){
-    return gulp.src('src/**/*.png')
-    .pipe(gulp.dest('build/images'));
-});
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('app/*.js', ['lint', 'scripts']);
-    gulp.watch('app/*.scss', ['sass']);
-
-    gulp.watch('src/*.scss', ['sass']);
-    gulp.watch('src/**/*.png', ['move_images']);
-    gulp.watch('src/*.html', ['move_html']);
-    gulp.watch('src/**').on('change', livereload.changed);
-});
-
-// Default Task
-gulp.task('default', ['lint', 'sass', 'bf', 'scripts', 'move_html', 'watch']);
+gulp.task('default', ['sass', 'browserify', 'connect', 'watch']);
