2a2b8f7f2c04cd722eb3ad01ec2b4bb1180440b5
grid <gridsane@gmail.com>
2014-11-23 17:22:54 +0500

    хелпер даты, даты в ForecastFullView

--- src/utils/template_helper.js
+++ src/utils/template_helper.js
@@ -1,4 +1,5 @@
 var Handlebars = require('hbsfy/runtime');
+var datef = require('datef');
 
 Handlebars.registerHelper('weatherIcon', function (icon) {
     return 'http://ekb.shri14.ru/icons/' + icon + '.svg';
@@ -8,10 +9,14 @@ Handlebars.registerHelper('moonIcon', function (moonCode) {
     return 'http://ekb.shri14.ru/icons/icon_moon_' + moonCode + '.svg';
 });
 
-// сравнение eq
 Handlebars.registerHelper('ifCond', function(v1, v2, options) {
     if(v1 === v2) {
         return options.fn(this);
     }
+
     return options.inverse(this);
+});
+
+Handlebars.registerHelper('datef', function (format, date) {
+    return datef(format, date);
 });
