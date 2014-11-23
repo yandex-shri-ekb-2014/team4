86f9516cc663829477409e49b6371319ca17c860
Moonshade <enigmatic.gate@gmail.com>
2014-11-23 14:29:53 +0500

    short forecast static template

--- src/views/forecast_short.js
+++ src/views/forecast_short.js
@@ -1,11 +1,15 @@
-var Backbone = require('backbone');
+var Backbone = require('backbone'),
+    forecastShortTemplate = require('../templates/forecast_short.hbs');
 
 var ForecastShortView = Backbone.View.extend({
+    template: forecastShortTemplate,
     initialize: function () {
         console.log("here is short view");
+        this.render();
     },
     render: function() {
-        console.log('render method');
+        console.log("short render");
+        console.log(this.template());
     }
 });
 
