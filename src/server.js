3b92cc5c85db92b14334b25a0e563ef80e08f47b
grid <gridsane@gmail.com>
2014-11-30 17:35:10 +0500

    server port for heroku

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 var path = require('path');
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
 
 app
   .use('/assets', express.static(__dirname + '/../assets'))
   .use(function (req, res, next) {
     res.sendFile(path.resolve(__dirname + '/../index.html'));
   })
-  .listen(port, function () {
-    console.log("Server listening on port " + port);
+  .listen(process.env.PORT, function () {
+    console.log("Server listening on port " + process.env.PORT);
   });
